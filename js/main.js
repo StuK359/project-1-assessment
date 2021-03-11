6a828e49a7f845a585355b2b97d3d27de1729bf7
Stuart Klingman <stuartk@avatek.com>
2021-03-11 15:46:29 -0800

    Corrected index.html to look for main.js and main.css

--- js/main.js
+++ js/main.js
@@ -1,76 +1,45 @@
-// // App State
-// let runningTotal;
-
-
-// // Cached Elements
-// const input1El = document.querySelector("#num1");
-// const input2El = document.querySelector("#num2");
-// const input3El = document.querySelector("#num3");
-// const totalEl = document.querySelector("h1 span");
-
-
-// // Event Listeners
-// document.querySelector("button").addEventListener("click", handleClick);
-
-
-// // Functions
-// function init() {
-//   runningTotal = 0;
-//   render();
-// }
-// init()
-// function render() {
-//   /* Alternative code; more compact and efficient */
-//   totalEl.textContent = (runningTotal >= 0 ? '+' : '') + runningTotal;   
-//   totalEl.style.color = runningTotal >= 0 ? "green" : "red";             
-
-//   // if (runningTotal >= 0) {
-//   //   totalEl.textContent = "+" + runningTotal;
-//   //   totalEl.style.color = "green";
-//   // } else {
-//   //   totalEl.textContent = runningTotal;
-//   //   totalEl.style.color = "red";
-//   // }
-// }
-// function handleClick() {
-//   let input1Value = parseInt(input1El.value);
-//   let input2Value = parseInt(input2El.value);
-//   let input3Value = parseInt(input3El.value);
-  
-//   runningTotal += (input1Value + input2Value + input3Value);
-//   input1El.value = input2El.value = input3El.value = 0;
-//   render();
-// }
-
 // App State
 let runningTotal;
+
+
 // Cached Elements
 const input1El = document.querySelector("#num1");
 const input2El = document.querySelector("#num2");
 const input3El = document.querySelector("#num3");
 const totalEl = document.querySelector("h1 span");
+
+
 // Event Listeners
 document.querySelector("button").addEventListener("click", handleClick);
+
+
 // Functions
 function init() {
   runningTotal = 0;
   render();
 }
-init();
+init()
 function render() {
-  if (runningTotal >= 0) {
-    totalEl.textContent = "+" + runningTotal;
-    totalEl.style.color = "green";
-  } else {
-    totalEl.textContent = runningTotal;
-    totalEl.style.color = "red";
-  }
+  /* Alternative code; more compact and efficient */
+  totalEl.textContent = (runningTotal >= 0 ? '+' : '') + runningTotal;   
+  totalEl.style.color = runningTotal >= 0 ? "green" : "red";             
+
+  // if (runningTotal >= 0) {
+  //   totalEl.textContent = "+" + runningTotal;
+  //   totalEl.style.color = "green";
+  // } else {
+  //   totalEl.textContent = runningTotal;
+  //   totalEl.style.color = "red";
+  // }
 }
 function handleClick() {
   let input1Value = parseInt(input1El.value);
   let input2Value = parseInt(input2El.value);
   let input3Value = parseInt(input3El.value);
+  
   runningTotal += (input1Value + input2Value + input3Value);
-  input1El.value = input2El.value = input3El.value = "";
+  input1El.value = input2El.value = input3El.value = 0;
   render();
 }
+
+
