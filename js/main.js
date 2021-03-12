5c492a5f460d090a6dcf3cef2ea753300a443b93
Stuart Klingman <stuartk@avatek.com>
2021-03-11 20:49:44 -0800

    Corner radius adjustment.

--- js/main.js
+++ js/main.js
@@ -11,8 +11,6 @@ const totalEl = document.getElementById("running-total");
 // Event Listeners
 document.getElementById("plus-button").addEventListener("click", handlePlusClick);
 document.getElementById("minus-button").addEventListener("click", handleMinusClick);
-// document.getElementById("plus-button").addEventListener("hover", inverseMinusColors);
-// document.getElementById("minus-button").addEventListener("hover", inversePlusColors);
 
 // Functions
 function init() {
@@ -24,38 +22,21 @@ function render() {
   /* Alternative code; more compact and efficient */
   totalEl.textContent = (runningTotal >= 0 ? '+' : '') + runningTotal;   
   totalEl.style.color = runningTotal >= 0 ? "green" : "red";             
-
-  // if (runningTotal >= 0) {
-  //   totalEl.textContent = "+" + runningTotal;
-  //   totalEl.style.color = "green";
-  // } else {
-  //   totalEl.textContent = runningTotal;
-  //   totalEl.style.color = "red";
-  // }
 }
+
 function handlePlusClick() {
   let input1Value = parseInt(input1El.value);
-  
-  runningTotal += (input1Value);
+  console.log(input1Value);
+  runningTotal += input1Value;
   render();
 }
 
 function handleMinusClick() {
   let input1Value = parseInt(input1El.value);
-  runningTotal -= (input1Value);
+  runningTotal -= input1Value;
   render();
 }
-// function inversePlusButtonColors() {
-//     var plusButton = document.getElementById("plus-button");
-//     plusButton.style.color = 'grey';
-//     plusButton.style.backgroundColor = "white";
-// }
 
-// function inverseMinusButtonColors() {
-//   var minusButton = document.getElementById("minus-button");
-//   minusButton.style.color = 'grey';
-//   minusButton.style.backgroundColor = "white";
-// }
 
 
 
